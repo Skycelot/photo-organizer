f6a115ea68a648fd6f07f01759e81bd54e3727d2
Mikhail Avakov <mikleavakov@gmail.com>
2018-08-09 21:36:10 +0300

    parsing jpeg exif data

--- src/test/java/ru/skycelot/photoorganizer/TestFeatures.java
+++ src/test/java/ru/skycelot/photoorganizer/TestFeatures.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import ru.skycelot.photoorganizer.conversion.json.FileEntityJsonConverter;
 import ru.skycelot.photoorganizer.conversion.json.JsonHelper;
 import ru.skycelot.photoorganizer.domain.FileEntity;
+import ru.skycelot.photoorganizer.jpeg.Segment;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
@@ -15,6 +16,8 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
+import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 
 public class TestFeatures {
@@ -25,9 +28,9 @@ public class TestFeatures {
         ByteArrayOutputStream jsonBytes = new ByteArrayOutputStream();
         ByteBuffer buffer = ByteBuffer.allocate(1024);
         ByteChannel fileChannel = Files.newByteChannel(jsonFile, StandardOpenOption.READ);
-        while(fileChannel.read(buffer) > 0) {
+        while (fileChannel.read(buffer) > 0) {
             buffer.flip();
-            while(buffer.hasRemaining()) {
+            while (buffer.hasRemaining()) {
                 jsonBytes.write(buffer.get());
             }
             buffer.clear();
@@ -37,5 +40,39 @@ public class TestFeatures {
         List<FileEntity> files = marshaller.unmarshall(json);
 
         int i = 0;
+    }
+
+    @Test
+    public void exif() throws IOException {
+        Path jpeg = Paths.get("/media/sf_Mount/_DSC0309.jpg");
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        ByteBuffer buffer = ByteBuffer.allocate(1024);
+        ByteChannel channel = Files.newByteChannel(jpeg);
+        while (channel.read(buffer) > 0) {
+            buffer.flip();
+            while (buffer.hasRemaining()) {
+                baos.write(buffer.get());
+            }
+            buffer.clear();
+        }
+        byte[] content = baos.toByteArray();
+        List<Segment> segments = new LinkedList<>();
+        int blockStart = 0;
+        while (blockStart < content.length) {
+            Segment segment = new Segment((short) (((content[blockStart] & 0xFF) << 8) | (content[blockStart + 1] & 0xFF)));
+            if (segment.hasLength()) {
+                int length = (((content[blockStart + 2] & 0xFF) << 8) | (content[blockStart + 3] & 0xFF)) - 2;
+                blockStart += 4;
+                if (length > 0) {
+                    segment.setContent(Arrays.copyOfRange(content, blockStart, blockStart + length));
+                    blockStart += length;
+                }
+            } else {
+                blockStart += 2;
+            }
+            segments.add(segment);
+        }
+
+        int z = 0;
     }
 }
